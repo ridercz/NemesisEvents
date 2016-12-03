55d0076d5159bc2bc5c5a01509b112c10a5cf578
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-12-03 18:46:42 +0100

    Temporary EventDetailDTO

--- src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/DTO/EventDetailDTO.cs
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/DTO/EventDetailDTO.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Riganti.Utils.Infrastructure.Core;
 
 namespace Altairis.NemesisEvents.BL.DTO
 {
-    public class EventDetailDTO
-    {
+    public class EventDetailDTO : IEntity<int> {
+
+        public int Id { get; set; }
     }
+
+    //TODO: implement this
 }
