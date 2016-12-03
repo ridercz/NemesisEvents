dc9059733fcb4febe4649d75f7d0961632350879
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-12-03 17:50:02 +0100

    Added user listing

---
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/DTO/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.NemesisEvents.DAL;
+
+namespace Altairis.NemesisEvents.BL.DTO {
+    public class UserDTO {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public string FullName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public EmailFrequency EmailFrequency { get; set; }
+
+        public int OwnedEventsCount { get; set; }
+
+        public int AttendancesCount { get; set; }
+
+    }
+}
---
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/Facades/UsersFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.NemesisEvents.BL.DTO;
+using Altairis.NemesisEvents.BL.Queries;
+
+namespace Altairis.NemesisEvents.BL.Facades {
+    public class UsersFacade : AppFacadeBase {
+
+        public Func<UsersQuery> UsersQueryFactory { get; set; }
+
+        public IList<UserDTO> List() {
+            using (var uow = this.UnitOfWorkProvider.Create()) {
+                var q = this.UsersQueryFactory();
+                return q.Execute();
+            }
+        }
+
+    }
+}
