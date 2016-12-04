84b40d553820624248ebf2918f52f241815cb0ec
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-12-04 14:03:01 +0100

    Changed routes to match old version

--- src/Altairis.NemesisEvents/Altairis.NemesisEvents.Web/DotvvmStartup.cs
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.Web/DotvvmStartup.cs
@@ -3,13 +3,10 @@ using DotVVM.Framework.Configuration;
 using DotVVM.Framework.ResourceManagement;
 using DotVVM.Framework.Routing;
 
-namespace Altairis.NemesisEvents.Web
-{
-    public class DotvvmStartup : IDotvvmStartup
-    {
+namespace Altairis.NemesisEvents.Web {
+    public class DotvvmStartup : IDotvvmStartup {
         // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
-        public void Configure(DotvvmConfiguration config, string applicationPath)
-        {
+        public void Configure(DotvvmConfiguration config, string applicationPath) {
             config.DefaultCulture = "cs-CZ";
 
             ConfigureRoutes(config, applicationPath);
@@ -17,37 +14,34 @@ namespace Altairis.NemesisEvents.Web
             ConfigureResources(config, applicationPath);
         }
 
-        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
-        {
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath) {
             config.RouteTable.Add("Default", "", "Views/Default.dothtml");
 
-            config.RouteTable.Add("Login", "prihlaseni", "Views/Account/Login.dothtml");
-            config.RouteTable.Add("ForgottenPassword", "zapomenute-heslo", "Views/Account/ForgottenPassword.dothtml");
-            config.RouteTable.Add("ResetPassword", "reset-hesla", "Views/Account/ResetPassword.dothtml");
-            config.RouteTable.Add("Register", "registrace", "Views/Account/Register.dothtml");
-            config.RouteTable.Add("VerifyEmail", "overeni-emailu", "Views/Account/VerifyEmail.dothtml");
+            config.RouteTable.Add("Login", "login", "Views/Account/Login.dothtml");
+            config.RouteTable.Add("ForgottenPassword", "reset", "Views/Account/ForgottenPassword.dothtml");
+            config.RouteTable.Add("ResetPassword", "reset/verify", "Views/Account/ResetPassword.dothtml");
+            config.RouteTable.Add("Register", "reg", "Views/Account/Register.dothtml");
+            config.RouteTable.Add("VerifyEmail", "reg/verify", "Views/Account/VerifyEmail.dothtml");
 
-            config.RouteTable.Add("EventDetail", "akce/{Id}", "Views/EventDetail.dothtml");
-            config.RouteTable.Add("Archive", "archiv", "Views/Archive.dothtml");
+            config.RouteTable.Add("EventDetail", "events/{Id}", "Views/EventDetail.dothtml");
+            config.RouteTable.Add("Archive", "events", "Views/Archive.dothtml");
+
 
             // Uncomment the following line to auto-register all dothtml files in the Views folder
-            // config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));    
+            // config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));
         }
 
-        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
-        {
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath) {
             // register code-only controls and markup controls
             config.Markup.AddMarkupControl("cc", "Menu", "Controls/Menu.dotcontrol");
             config.Markup.AddMarkupControl("cc", "Alerts", "Controls/Alerts.dotcontrol");
         }
 
-        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
-        {
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath) {
             // register custom resources and adjust paths to the built-in resources
-            config.Resources.Register("bootstrap", new ScriptResource()
-            {
+            config.Resources.Register("bootstrap", new ScriptResource() {
                 Url = "~/js/bootstrap.min.js",
-                Dependencies = new [] { "jquery" }
+                Dependencies = new[] { "jquery" }
             });
         }
     }
