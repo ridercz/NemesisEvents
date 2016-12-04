6eb040bc02823a536ea4b837480d5332071ec70b
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-12-04 12:26:42 +0100

    Added attachment mapping

---
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/Mapping/AttachmentMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.NemesisEvents.BL.DTO;
+using Altairis.NemesisEvents.DAL;
+using AutoMapper;
+
+namespace Altairis.NemesisEvents.BL.Mapping
+{
+    public class AttachmentMapping : IMapping {
+        public void Map(IMapperConfigurationExpression cfg) {
+            cfg.CreateMap<Attachment, AttachmentDTO>();
+            cfg.CreateMap<AttachmentDTO, Attachment>();
+        }
+    }
+}
