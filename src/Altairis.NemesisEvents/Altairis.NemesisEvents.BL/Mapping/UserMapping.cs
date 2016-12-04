959bf115e275595ff0a0f7329f1faa9971bf8241
Michal Altair Valasek <michal.valasek@altairis.cz>
2016-12-04 15:00:57 +0100

    Faking lazy loading

--- src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/Mapping/UserMapping.cs
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/Mapping/UserMapping.cs
@@ -4,6 +4,7 @@ using Altairis.NemesisEvents.DAL;
 using AutoMapper;
 using Riganti.Utils.Infrastructure.Core;
 using Riganti.Utils.Infrastructure.AutoMapper;
+using Riganti.Utils.Infrastructure.EntityFrameworkCore;
 
 namespace Altairis.NemesisEvents.BL.Mapping {
     public class UserMapping : IMapping {
@@ -17,9 +18,17 @@ namespace Altairis.NemesisEvents.BL.Mapping {
         public void Map(IMapperConfigurationExpression cfg) {
             cfg.CreateMap<User, UserDTO>();
             cfg.CreateMap<User, PublicProfileDTO>()
+                .BeforeMap((s, d) => {  // EF Core does not support lazy loading yet
+                    EntityFrameworkUnitOfWork.TryGetDbContext(uowProvider).Entry(s).Collection(x => x.WatchedAreas).Load();
+                    EntityFrameworkUnitOfWork.TryGetDbContext(uowProvider).Entry(s).Collection(x => x.WatchedTags).Load();
+                })
                 .ForMember(e => e.WatchedAreaIds, m => m.MapFrom(e => e.WatchedAreas.Select(a => a.AreaId)))
                 .ForMember(e => e.WatchedTagIds, m => m.MapFrom(e => e.WatchedTags.Select(a => a.TagId)));
             cfg.CreateMap<PublicProfileDTO, User>()
+                .BeforeMap((s, d) => {  // EF Core does not support lazy loading yet
+                    EntityFrameworkUnitOfWork.TryGetDbContext(uowProvider).Entry(d).Collection(x => x.WatchedAreas).Load();
+                    EntityFrameworkUnitOfWork.TryGetDbContext(uowProvider).Entry(d).Collection(x => x.WatchedTags).Load();
+                })
                 .ForMember(e => e.WatchedAreas, m => m.DropAndCreateCollection(uowProvider, x => x.WatchedAreaIds, x => new UserArea { AreaId = x }))
                 .ForMember(e => e.WatchedTags, m => m.DropAndCreateCollection(uowProvider, x => x.WatchedTagIds, x => new UserTag { TagId = x }));
 
