2488a765373b63ad06da1998c198087ca8ca71c6
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-03-25 17:42:36 +0100

    Added SelectListItemDTO

---
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.BL/DTO/SelectListItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altairis.NemesisEvents.BL.DTO {
+    public class SelectListItemDTO<TValue, TDisplay> {
+
+        public TValue Value { get; set; }
+
+        public TDisplay Display { get; set; }
+
+    }
+}
