921355240334b7076d5ad4f83217c915735e66c0
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-03-25 20:46:57 +0100

    Added new event form

--- src/Altairis.NemesisEvents/Altairis.NemesisEvents.Web/ViewModels/Admin/EventsNewViewModel.cs
+++ src/Altairis.NemesisEvents/Altairis.NemesisEvents.Web/ViewModels/Admin/EventsNewViewModel.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DotVVM.Framework.ViewModel;
+using Altairis.NemesisEvents.BL.DTO;
+using Altairis.NemesisEvents.BL.Facades;
 
-namespace Altairis.NemesisEvents.Web.ViewModels.Admin
-{
-	public class EventsNewViewModel : MasterPageViewModel
-	{
-	}
+namespace Altairis.NemesisEvents.Web.ViewModels.Admin {
+    public class EventsNewViewModel : NewViewModelBase<EventsFacade, EventDetailDTO, int> {
+
+        // Injected services
+
+        [Bind(Direction.None)]
+        public BaseListsFacade BaseListsFacade { get; set; }
+
+        // Page infrastructure
+
+        protected override string ContinuePageRouteName => "AdminEvents";
+
+        protected override string GetPageTitle() => "Vytvoøení nové akce";
+
+        // Binding properties
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public List<UserBasicDTO> Users => this.BaseListsFacade.GetOrganizerOrAdminUsers();
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public List<VenueBasicDTO> Venues => this.BaseListsFacade.GetVenues();
+
+        [Bind(Direction.ServerToClientFirstRequest)]
+        public List<TagDTO> Tags => this.BaseListsFacade.GetTags();
+
+
+    }
 }
 
